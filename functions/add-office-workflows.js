eebc46406352f7f8b935aa017b88bfd0e0f6544c
Kevin Wang <rocwang0316@gmail.com>
2019-04-29 22:55:53 -0700

    Allow the function to return a department list.

--- functions/add-office-workflows.js
+++ functions/add-office-workflows.js
@@ -6,7 +6,7 @@ const { queryAsync, format } = require('@kevinwang0316/mysql-helper');
 
 const wrapper = require('../middlewares/wrapper');
 
-const QUERY_SQL = 'SELECT id FROM ?? WHERE id IN (?)';
+const QUERY_SQL = 'SELECT id, department FROM ?? WHERE id IN (?)';
 const INSERT_SQL = 'INSERT INTO ?? (id, status, name, description, listId, region, workflowType, assigneUse, tenantUrl) VALUES (?)';
 
 const getInsertSQL = (workflows) => {
@@ -21,12 +21,16 @@ const handler = async (event, context) => {
   const { workflows } = JSON.parse(event.body);
 
   try {
-    // Get a list ids for existed rows
+    // Get a list ids for existed rows and their department information
     const { rows } = await trackExecTime('MySQL Query Latency', () => queryAsync(QUERY_SQL, [process.env.OFFICEWORKFLOW_TABLE, Object.keys(workflows)]));
-    // Remove existed recoreds from workflos
-    rows.forEach(({ id }) => delete workflows[id]);
+    // Remove existed recoreds from workflos and make a { id: department } object to return
+    const departments = {};
+    rows.forEach(({ id, department }) => {
+      departments[id] = department;
+      delete workflows[id];
+    });
     if (Object.keys(workflows).length !== 0) await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(getInsertSQL(workflows)));
-    return { statusCode: 200 };
+    return { statusCode: 200, body: JSON.stringify({ departments }) };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
     return { statusCode: 500 };
