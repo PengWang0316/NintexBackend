0443cd82b56e1db556034439620e277be1090f86
Kevin Wang <rocwang0316@gmail.com>
2019-05-12 19:30:09 -0700

    Added first unit test for the function.

--- __tests__/unit_tests/send-workflow-ids-stepfunction.js
+++ __tests__/unit_tests/send-workflow-ids-stepfunction.js
@@ -1,9 +1,12 @@
-import AWS from 'asw-sdk';
+import AWS from 'aws-sdk';
 import log from '@kevinwang0316/log';
+
+import sendWorkflowIds from '../../functions/libs/send-workflow-ids-stepfunction';
 
 require('../helpers/initailEnvsForUnitTest');
 
-const mockStepFunction = jest.fn();
+const mockStartExecution = jest.fn();
+const mockStepFunction = { startExecution: mockStartExecution };
 
 jest.mock('aws-sdk', () => ({
   config: { update: jest.fn() },
@@ -13,7 +16,12 @@ jest.mock('aws-sdk', () => ({
 describe('send-workflow-ids-stepfunction', () => {
   beforeEach(() => jest.clearAllMocks());
 
-  test('sendWorkflowIds without error', () => {
+  test('sendWorkflowIds -> workflows equal zero without error', async () => {
+    const workflows = [];
+    const keys = { key1: 1, key2: 2 };
 
+    await sendWorkflowIds(workflows, keys);
+
+    expect(mockStartExecution).not.toHaveBeenCalled();
   });
 });
