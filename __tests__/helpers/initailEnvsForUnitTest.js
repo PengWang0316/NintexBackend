044fbc9b1fb01937b53fc9ce930713a67f877db2
Kevin Wang <rocwang0316@gmail.com>
2019-05-13 12:24:42 -0700

    Added one more variable.

--- __tests__/helpers/initailEnvsForUnitTest.js
+++ __tests__/helpers/initailEnvsForUnitTest.js
@@ -1,6 +1,7 @@
 process.env = {
   STAGE: 'stage',
   AWS_REGION: 'us-west-2',
-  WORKFLOW_BATCH_NUM: 10,
+  WORKFLOW_BATCH_NUM: 3,
   MESSAGE_DELAY_SECOND: 10,
+  FETCH_HEALTH_SCORE_SF: 'function arn',
 };
