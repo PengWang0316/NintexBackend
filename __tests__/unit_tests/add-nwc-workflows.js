54715c75b352150eaf730aa5802ec1f36c6f85c9
Kevin Wang <rocwang0316@gmail.com>
2019-05-17 18:57:50 -0700

    Added one test case

---
+++ __tests__/unit_tests/add-nwc-workflows.js
@@ -0,0 +1,36 @@
+import { error } from '@kevinwang0316/log';
+import { trackExecTime } from '@kevinwang0316/cloudwatch';
+import { queryAsync } from '@kevinwang0316/mysql-helper';
+
+import sendToStepfunction from '../../functions/libs/send-workflow-ids-stepfunction';
+
+import { handler } from '../../functions/add-nwc-workflows';
+
+require('../helpers/initailEnvsForUnitTest');
+
+jest.mock('@kevinwang0316/log', () => ({ error: jest.fn() }));
+jest.mock('@kevinwang0316/cloudwatch', () => ({ trackExecTime: jest.fn().mockImplementation((string, fn) => fn()) }));
+jest.mock('@kevinwang0316/mysql-helper', () => ({ queryAsync: jest.fn() }));
+jest.mock('../../functions/libs/send-workflow-ids-stepfunction', () => jest.fn().mockResolvedValue(true));
+jest.mock('../../middlewares/wrapper', () => jest.fn().mockImplementation(func => func));
+
+const QUERY_SQL = 'INSERT INTO ?? (id, isPublished, name, authorName, authorId, authorEmail, created, eventConfiguration, eventType, isActive, lastPublished, publishedType, publishedId, tenantUrl, description) VALUES ?';
+
+describe('add-nwc-workflows', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  test('run handler -> without error', async () => {
+    const event = { body: JSON.stringify({ workflows: 'workflows', keys: 'keys' }) };
+    const context = { functionName: 'functionName' };
+
+    const result = await handler(event, context);
+
+    expect(trackExecTime).toHaveBeenCalledTimes(1);
+    expect(queryAsync).toHaveBeenCalledTimes(1);
+    expect(queryAsync).toHaveBeenLastCalledWith(QUERY_SQL, [process.env.NWCWORKFLOW_TABLE, 'workflows']);
+    expect(sendToStepfunction).toHaveBeenCalledTimes(1);
+    expect(sendToStepfunction).toHaveBeenLastCalledWith('workflows', 'keys');
+    expect(error).not.toHaveBeenCalled();
+    expect(result).toEqual({ statusCode: 200 });
+  });
+});
