11438dbdb2bbb88869a8f90094ccb85435fc3dbd
Kevin Wang <rocwang0316@gmail.com>
2019-05-25 14:29:06 -0700

    Updated to use the lib code to get sql.

--- functions/add-workflows.js
+++ functions/add-workflows.js
@@ -1,30 +1,20 @@
-
-
 const log = require('@kevinwang0316/log');
 const { trackExecTime } = require('@kevinwang0316/cloudwatch');
-const { queryAsync, format } = require('@kevinwang0316/mysql-helper');
+const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
 const wrapper = require('../middlewares/wrapper');
+const generateInsertSQL = require('./libs/generateInsertSQL');
 
 const INSERT_SQL = `INSERT INTO ?? (lhProductType, lhDataSource, lhSiteList, id, workflowVersion,
                     authorDisplayName, authorEmail, tenantId, environmentId, locationId, locationName,
                     locationPath, locationUrl, assignedUse, email, location1, location2, sliceDate, location3,
                     workflowType, workflowName, publishDate, url, publisher, home, userId) VALUES (?)`;
 
-const getInsertSQL = (workflows, userId) => {
-  let queries = '';
-  workflows.forEach((workflow) => {
-    workflow.push(userId);
-    queries += `${format(INSERT_SQL, [process.env.WORKFLOWS_TABLE, workflow])};`;
-  });
-  return queries;
-};
-
 const handler = async (event, context) => {
   const { workflows } = JSON.parse(event.body);
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
   try {
-    await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(getInsertSQL(workflows, sub)));
+    await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(generateInsertSQL(INSERT_SQL, workflows, process.env.WORKFLOWS_TABLE, sub)));
     return { statusCode: 200 };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
