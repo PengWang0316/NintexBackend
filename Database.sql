5d048f91c170a053d205209046e02edc83c98350
Kevin Wang <rocwang0316@gmail.com>
2019-05-26 14:39:06 -0700

    Added the fetch-instance-count function.

--- Database.sql
+++ Database.sql
@@ -118,8 +118,8 @@ CREATE TABLE Actions (
 CREATE INDEX userid_publisher_action ON Actions(userId, publisher, actionUse, actionName);
 
 /* Some query SQLs for the dashboard */
-SELECT COUNT(*) FROM Workflows WHERE userId = '';
-SELECT SUM(instanceCount) FROM Instances WHERE userId = '';
+SELECT COUNT(*) count FROM Workflows WHERE userId = '';
+SELECT SUM(instanceCount) count FROM Instances WHERE userId = '';
 SELECT COUNT(DISTINCT publisher) FROM Actions WHERE userId = '';
 SELECT COUNT(IF(status = 'Completed', 1, NULL)) completed, COUNT(IF(status = 'Failed', 1, NULL)) failed, COUNT(IF(status = 'Started', 1, NULL)) started FROM Instances WHERE userId = '';
 SELECT statusDate, COUNT(IF(status = 'Completed', 1, NULL)) completed, COUNT(IF(status = 'Failed', 1, NULL)) failed, COUNT(IF(status = 'Started', 1, NULL)) started FROM Instances WHERE userId = '' GROUP BY statusDate;
---
+++ functions/fetch-instance-count.js
@@ -0,0 +1,20 @@
+const log = require('@kevinwang0316/log');
+const { trackExecTime } = require('@kevinwang0316/cloudwatch');
+const { queryAsync } = require('@kevinwang0316/mysql-helper');
+
+const wrapper = require('../middlewares/wrapper');
+
+const INSERT_SQL = 'SELECT SUM(instanceCount) count FROM ?? WHERE userId = ?';
+
+const handler = async (event, context) => {
+  const { requestContext: { authorizer: { claims: { sub } } } } = event;
+  try {
+    const { rows } = await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(INSERT_SQL, [process.env.INSTANCES_TABLE, sub]));
+    return { statusCode: 200, body: JSON.stringify(rows[0]) };
+  } catch (err) {
+    log.error(`${context.functionName}: ${err}`);
+    return { statusCode: 500 };
+  }
+};
+
+module.exports.handler = wrapper(handler);
