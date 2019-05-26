d53eed8a930968b86a932bda1c816ffa61c62c77
Kevin Wang <rocwang0316@gmail.com>
2019-05-26 14:52:35 -0700

    Updated a variable name.

--- functions/fetch-instance-count.js
+++ functions/fetch-instance-count.js
@@ -4,12 +4,12 @@ const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
 const wrapper = require('../middlewares/wrapper');
 
-const INSERT_SQL = 'SELECT SUM(instanceCount) count FROM ?? WHERE userId = ?';
+const QUERY_SQL = 'SELECT SUM(instanceCount) count FROM ?? WHERE userId = ?';
 
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
   try {
-    const { rows } = await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(INSERT_SQL, [process.env.INSTANCES_TABLE, sub]));
+    const { rows } = await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(QUERY_SQL, [process.env.INSTANCES_TABLE, sub]));
     return { statusCode: 200, body: JSON.stringify(rows[0]) };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
