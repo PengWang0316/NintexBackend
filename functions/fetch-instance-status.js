875f0bae4ae1b46a39426d26e320fd166dc97065
Kevin Wang <rocwang0316@gmail.com>
2019-05-26 16:26:45 -0700

    Added the fetch-instance-status function.

---
+++ functions/fetch-instance-status.js
@@ -0,0 +1,26 @@
+const log = require('@kevinwang0316/log');
+const { trackExecTime } = require('@kevinwang0316/cloudwatch');
+const { queryAsync } = require('@kevinwang0316/mysql-helper');
+
+const wrapper = require('../middlewares/wrapper');
+
+const QUERY_SQL = `SELECT SUM(IF(status = 'Completed', instanceCount, NULL)) completed,
+                   SUM(IF(status = 'Failed', instanceCount, NULL)) failed,
+                   SUM(IF(status = 'Started', instanceCount, NULL)) started,
+                   SUM(IF(status = 'Faulting', instanceCount, NULL)) faulting,
+                   SUM(IF(status = 'Running', instanceCount, NULL)) running,
+                   SUM(IF(status = 'Terminated', instanceCount, NULL)) terminatedInstance,
+                   SUM(IF(status = 'Cancelled', instanceCount, NULL)) cancelled FROM ?? WHERE userId = ?`;
+
+const handler = async (event, context) => {
+  const { requestContext: { authorizer: { claims: { sub } } } } = event;
+  try {
+    const { rows } = await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(QUERY_SQL, [process.env.INSTANCES_TABLE, sub]));
+    return { statusCode: 200, body: JSON.stringify(rows[0]) };
+  } catch (err) {
+    log.error(`${context.functionName}: ${err}`);
+    return { statusCode: 500 };
+  }
+};
+
+module.exports.handler = wrapper(handler);
