6b9a85b7b936288995f5233b0810cb738e2d05c1
Kevin Wang <rocwang0316@gmail.com>
2019-05-26 16:46:14 -0700

    Updated the query statement.

--- functions/fetch-instance-status.js
+++ functions/fetch-instance-status.js
@@ -4,13 +4,13 @@ const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
 const wrapper = require('../middlewares/wrapper');
 
-const QUERY_SQL = `SELECT SUM(IF(status = 'Completed', instanceCount, NULL)) completed,
-                   SUM(IF(status = 'Failed', instanceCount, NULL)) failed,
-                   SUM(IF(status = 'Started', instanceCount, NULL)) started,
-                   SUM(IF(status = 'Faulting', instanceCount, NULL)) faulting,
-                   SUM(IF(status = 'Running', instanceCount, NULL)) running,
-                   SUM(IF(status = 'Terminated', instanceCount, NULL)) terminatedInstance,
-                   SUM(IF(status = 'Cancelled', instanceCount, NULL)) cancelled FROM ?? WHERE userId = ?`;
+const QUERY_SQL = `SELECT SUM(IF(status = 'Completed', instanceCount, 0)) completed,
+                   SUM(IF(status = 'Failed', instanceCount, 0)) failed,
+                   SUM(IF(status = 'Started', instanceCount, 0)) started,
+                   SUM(IF(status = 'Faulting', instanceCount, 0)) faulting,
+                   SUM(IF(status = 'Running', instanceCount, 0)) running,
+                   SUM(IF(status = 'Terminated', instanceCount, 0)) terminatedInstance,
+                   SUM(IF(status = 'Cancelled', instanceCount, 0)) cancelled FROM ?? WHERE userId = ?`;
 
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
