5c07c3dc0cfda7232645f7f5e518a971b846d9ff
Kevin Wang <rocwang0316@gmail.com>
2019-05-26 14:39:26 -0700

    Updated the format for the return value.

--- functions/fetch-workflow-count.js
+++ functions/fetch-workflow-count.js
@@ -9,8 +9,8 @@ const INSERT_SQL = 'SELECT COUNT(*) count FROM ?? WHERE userId = ?';
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
   try {
-    const result = await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(INSERT_SQL, [process.env.WORKFLOWS_TABLE, sub]));
-    return { statusCode: 200, body: JSON.stringify(result) };
+    const { rows } = await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(INSERT_SQL, [process.env.WORKFLOWS_TABLE, sub]));
+    return { statusCode: 200, body: JSON.stringify(rows[0]) };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
     return { statusCode: 500 };
