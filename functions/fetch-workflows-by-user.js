0897774e7fd026eca629ac4adb1f819ccb53a405
Kevin Wang <rocwang0316@gmail.com>
2019-06-09 14:21:08 -0700

    Removed Office workflow call

--- functions/fetch-workflows-by-user.js
+++ functions/fetch-workflows-by-user.js
@@ -6,7 +6,7 @@ const wrapper = require('../middlewares/wrapper');
 
 const QUERY_WORKFLOWS_SQL = 'SELECT id as workflowId, workflowName, publishDate, publisher, locationPath, tags FROM ?? Where userId=?';
 const QUERY_NWCWORKFLOW_SQL = 'SELECT id as workflowId, name as workflowName, created as publishDate, authorName as publisher, tags, isActive, tenantUrl as tenant FROM ?? WHERE userId=?';
-const QUERY_OFFIC_WORKFLOW_SQL = 'SELECT id as workflowId, name as workflowName, tags, tenantUrl as tenant FROM ?? WHERE userId=?';
+// const QUERY_OFFIC_WORKFLOW_SQL = 'SELECT id as workflowId, name as workflowName, tags, tenantUrl as tenant FROM ?? WHERE userId=?';
 
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
@@ -15,7 +15,7 @@ const handler = async (event, context) => {
     const data = await Promise.all([
       trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_WORKFLOWS_SQL, [process.env.WORKFLOWS_TABLE, sub])),
       trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_NWCWORKFLOW_SQL, [process.env.NWCWORKFLOW_TABLE, sub])),
-      trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_OFFIC_WORKFLOW_SQL, [process.env.OFFICEWORKFLOW_TABLE, sub])),
+      // trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_OFFIC_WORKFLOW_SQL, [process.env.OFFICEWORKFLOW_TABLE, sub])),
     ]);
     return {
       statusCode: 200,
