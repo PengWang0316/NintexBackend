f18d2c619e0d10826bd1fdd01cc46edb1d455919
Kevin Wang <rocwang0316@gmail.com>
2019-06-09 12:43:50 -0700

    Updated the function to fetch data from two more table.

--- functions/fetch-workflows-by-user.js
+++ functions/fetch-workflows-by-user.js
@@ -4,13 +4,23 @@ const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
 const wrapper = require('../middlewares/wrapper');
 
-const QUERY_SQL = 'SELECT id as workflowId, workflowName, publishDate, publisher, locationPath, tags FROM ?? Where userId=?';
+const QUERY_WORKFLOWS_SQL = 'SELECT id as workflowId, workflowName, publishDate, publisher, locationPath, tags FROM ?? Where userId=?';
+const QUERY_NWCWORKFLOW_SQL = 'SELECT id as workflowId, name as workflowName, created as publishDate, authorName as publisher, tags, isActive, tenantUrl as tenant FROM ?? WHERE userId=?';
+const QUERY_OFFIC_WORKFLOW_SQL = 'SELECT id as workflowId, name as workflowName, tags, tenantUrl as tenant FROM ?? WHERE userId=?';
 
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
   try {
-    const { rows } = await trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_SQL, [process.env.WORKFLOWS_TABLE, sub]));
-    return { statusCode: 200, body: JSON.stringify(rows) };
+    // const { rows } = await trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_WORKFLOWS_SQL, [process.env.WORKFLOWS_TABLE, sub]));
+    const data = await Promise.all([
+      trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_WORKFLOWS_SQL, [process.env.WORKFLOWS_TABLE, sub])),
+      trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_NWCWORKFLOW_SQL, [process.env.NWCWORKFLOW_TABLE, sub])),
+      trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_OFFIC_WORKFLOW_SQL, [process.env.OFFICEWORKFLOW_TABLE, sub])),
+    ]);
+    return {
+      statusCode: 200,
+      body: JSON.stringify([...data[0].rows, ...data[1].rows, ...data[2].rows]),
+    };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
     return { statusCode: 500 };
