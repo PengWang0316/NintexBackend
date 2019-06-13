48a60ad639ea5ba62c0c1fb3c1090556260b50eb
Kevin Wang <rocwang0316@gmail.com>
2019-06-12 23:32:49 -0700

    Added the id to the return value.

--- functions/fetch-nwckeys.js
+++ functions/fetch-nwckeys.js
@@ -4,12 +4,12 @@ const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
 const wrapper = require('../middlewares/wrapper');
 
-const QUERY_SQL = 'SELECT tenant, apiKey FROM ?? WHERE userId = ?';
+const QUERY_SQL = 'SELECT id, tenant, apiKey FROM ?? WHERE userId = ?';
 
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
   try {
-    const { rows } = await trackExecTime('MySQL Batch Query Latency', () => queryAsync(QUERY_SQL, [process.env.NWCKEYS_TABLE, sub]));
+    const { rows } = await trackExecTime('MySQL Query Latency', () => queryAsync(QUERY_SQL, [process.env.NWCKEYS_TABLE, sub]));
     return { statusCode: 200, body: JSON.stringify(rows) };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
