92b642455c07afc5fdad6bc4244fb573bc131255
Kevin Wang <rocwang0316@gmail.com>
2019-06-13 23:17:28 -0700

    Added the fetch-officekey funciton

---
+++ functions/fetch-officekeys.js
@@ -0,0 +1,20 @@
+const log = require('@kevinwang0316/log');
+const { trackExecTime } = require('@kevinwang0316/cloudwatch');
+const { queryAsync } = require('@kevinwang0316/mysql-helper');
+
+const wrapper = require('../middlewares/wrapper');
+
+const QUERY_SQL = 'SELECT id, endpoint, apiKey, cookie FROM ?? WHERE userId = ?';
+
+const handler = async (event, context) => {
+  const { requestContext: { authorizer: { claims: { sub } } } } = event;
+  try {
+    const { rows } = await trackExecTime('MySQL Query Latency', () => queryAsync(QUERY_SQL, [process.env.OFFICEKEYS_TABLE, sub]));
+    return { statusCode: 200, body: JSON.stringify(rows) };
+  } catch (err) {
+    log.error(`${context.functionName}: ${err}`);
+    return { statusCode: 500 };
+  }
+};
+
+module.exports.handler = wrapper(handler);
