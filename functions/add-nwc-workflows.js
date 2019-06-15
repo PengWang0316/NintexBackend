a6522624189c3e9c697f953b8aba860442757338
Kevin Wang <rocwang0316@gmail.com>
2019-06-14 21:40:36 -0700

    Added the add-nwc-workflows function.

--- functions/add-nwc-workflows.js
+++ functions/add-nwc-workflows.js
@@ -1,26 +1,24 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-plusplus */
-'use strict';
-
 const log = require('@kevinwang0316/log');
 const { trackExecTime } = require('@kevinwang0316/cloudwatch');
 const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
-const sendToStepfunction = require('./libs/send-workflow-ids-stepfunction');
-
 const wrapper = require('../middlewares/wrapper');
 
-const QUERY_SQL = 'INSERT INTO ?? (id, isPublished, name, authorName, authorId, authorEmail, created, eventConfiguration, eventType, isActive, lastPublished, publishedType, publishedId, tenantUrl, description) VALUES ?';
+const QUERY_SQL = 'INSERT INTO ?? (id, isPublished, name, authorName, authorId, authorEmail, created, eventConfiguration, eventType, isActive, lastPublished, publishedType, publishedId, tenantUrl, description, userId) VALUES ?';
 
+/* If some workflow has already exsit in the database, this function will fail due to the same id.
+ * We will rely on the auto fetching to catch up new recored under this situation.
+ */
 const handler = async (event, context) => {
-  const { workflows, keys } = JSON.parse(event.body);
+  const { requestContext: { authorizer: { claims: { sub } } } } = event;
+  const { workflows } = JSON.parse(event.body);
+  // Add the user id in
+  workflows.forEach(workflow => workflow.push(sub));
   try {
     await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(
       QUERY_SQL,
       [process.env.NWCWORKFLOW_TABLE, workflows],
     ));
-
-    await sendToStepfunction(workflows, keys);
     return { statusCode: 200 };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
