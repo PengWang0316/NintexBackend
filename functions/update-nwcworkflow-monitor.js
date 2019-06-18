447a7be04cb0369880e4119796a58c109612597e
Kevin Wang <rocwang0316@gmail.com>
2019-06-18 16:15:03 -0700

    Adde the update-nwcworkflow-monitor function.

---
+++ functions/update-nwcworkflow-monitor.js
@@ -0,0 +1,21 @@
+const log = require('@kevinwang0316/log');
+const { trackExecTime } = require('@kevinwang0316/cloudwatch');
+const { queryAsync } = require('@kevinwang0316/mysql-helper');
+
+const wrapper = require('../middlewares/wrapper');
+
+const UPDATE_SQL = 'UPDATE ?? SET isMonitored = ? WHERE id = ? AND userId = ?';
+
+const handler = async (event, context) => {
+  const { requestContext: { authorizer: { claims: { sub } } } } = event;
+  const { workflowId, isMonitored } = JSON.parse(event.body);
+  try {
+    await trackExecTime('MySQL UPDATE Latency', () => queryAsync(UPDATE_SQL, [process.env.NWCWORKFLOW_TABLE, isMonitored, workflowId, sub]));
+    return { statusCode: 200 };
+  } catch (err) {
+    log.error(`${context.functionName}: ${err}`);
+    return { statusCode: 500 };
+  }
+};
+
+module.exports.handler = wrapper(handler);
