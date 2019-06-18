59d9181a4d13ac6a82715c4386939c23a1cdf6a0
Kevin Wang <rocwang0316@gmail.com>
2019-06-17 20:10:45 -0700

    Modified the function to take both initial add nwc call and auto fetching

--- functions/add-nwc-workflows.js
+++ functions/add-nwc-workflows.js
@@ -1,24 +1,39 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-plusplus */
+const AWS = require('aws-sdk');
+const axios = require('axios');
 const log = require('@kevinwang0316/log');
-const { trackExecTime } = require('@kevinwang0316/cloudwatch');
 const { queryAsync } = require('@kevinwang0316/mysql-helper');
 
+const exportDeactiveImportInsertNWC = require('./libs/ExportDeactiveImportInsertNWC');
 const wrapper = require('../middlewares/wrapper');
 
-const QUERY_SQL = 'INSERT INTO ?? (id, isPublished, name, authorName, authorId, authorEmail, created, eventConfiguration, eventType, isActive, lastPublished, publishedType, publishedId, tenantUrl, description, userId) VALUES ?';
+AWS.config.update({ region: process.env.AWS_REGION });
+const stepFunctions = new AWS.StepFunctions();
 
 /* If some workflow has already exsit in the database, this function will fail due to the same id.
  * We will rely on the auto fetching to catch up new recored under this situation.
  */
 const handler = async (event, context) => {
   const { requestContext: { authorizer: { claims: { sub } } } } = event;
-  const { workflows } = JSON.parse(event.body);
-  // Add the user id in
-  workflows.forEach(workflow => workflow.push(sub));
+  const { workflows, key, isAutoFetching } = JSON.parse(event.body);
+
   try {
-    await trackExecTime('MySQL Batch Insert Latency', () => queryAsync(
-      QUERY_SQL,
-      [process.env.NWCWORKFLOW_TABLE, workflows],
-    ));
+    if (workflows.length < process.env.LIMIT_BATCH_NUMBER) await exportDeactiveImportInsertNWC(workflows, sub, key, queryAsync, isAutoFetching);
+    else { // If we have too many workflows, send the workload to different step functions
+      for (let i = 0, { length } = workflows; i < length / process.env.BATCH_NUMBER; i++) {
+        await stepFunctions.startExecution({
+          stateMachineArn: process.env.EXPORT_IMPORT_SF,
+          input: JSON.stringify({
+            workflows: workflows.slice(i * process.env.BATCH_NUMBER, i * process.env.BATCH_NUMBER + process.env.BATCH_NUMBER),
+            sub,
+            key,
+            waitTime: process.env.DELAY_SECOND * i,
+            isAutoFetching,
+          }),
+        }).promise();
+      }
+    }
     return { statusCode: 200 };
   } catch (err) {
     log.error(`${context.functionName}: ${err}`);
